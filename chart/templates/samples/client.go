d43bb797aa0fd3d91e689658cec3171bb58432a7
Diego Braga <diego.braga86@gmail.com>
2024-07-02 09:45:44 +0200

    feat: first commit

---
+++ chart/templates/samples/client.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "client.html")
+	})
+
+	http.ListenAndServe(":80", nil)
+}
